e6e6e23fbf5239d65e36eaf56f1702a396b6a2be
Tamas Tudor-Ciprian (uig60821) <tudor-ciprian.tamas@continental-corporation.com>
2024-09-10 12:09:34 +0300

    fixed mining at any y bug

--- what_is_necesary/defines.py
+++ what_is_necesary/defines.py
@@ -11,4 +11,4 @@ CHARACTER_COLOR = (255, 179, 0)
 
 
 def comp_dist (obj1,obj2,distance):
-    return abs(obj1.x - obj2.x) <distance and abs(obj1.y-obj2.y)
+    return abs(obj1.x - obj2.x) <distance and abs(obj1.y-obj2.y) < distance
